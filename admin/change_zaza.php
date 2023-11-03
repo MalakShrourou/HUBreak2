dea6f834cff089b20b1d0e04e09d22feb32c936c
Baseljamal <70074852+Baseljamal@users.noreply.github.com>
2023-11-03 22:39:53 +0300

    Add files via upload

---
+++ admin/change_zaza.php
@@ -0,0 +1,446 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Zaza - Change Price</title>
+    <meta charset=utf8>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', Times, serif;
+            box-sizing: border-box;
+        }
+
+        body {
+            color: rgb(124, 17, 17);
+            background-attachment: fixed;
+        }
+
+        .menu-bar {
+            background: #f18b05;
+            text-align: right;
+            height: 40px;
+            margin-right: 10%;
+            padding-right: 5px;
+            font-size: large;
+        }
+
+        .menu-bar ul {
+            display: inline-flex;
+            list-style: none;
+            color: #fff;
+        }
+
+        .menu-bar ul li {
+            width: 120px;
+            margin: 0px;
+            padding: 10px 0px;
+            text-align: center;
+        }
+
+        .menu-bar ul li a {
+            text-decoration: none;
+            color: #fff;
+            padding-right: 10px;
+        }
+
+        .menu,
+        .user {
+            display: none;
+        }
+
+        .menu-bar ul li:hover {
+            background-color: #fda025;
+        }
+
+        .menu-bar ul li:hover .menu,
+        .menu-bar ul li:hover .user {
+            display: block;
+            position: absolute;
+            background-color: #FBB202;
+            margin-top: 10px;
+            margin-left: -20px;
+        }
+
+        .menu-bar ul li:hover .menu ul,
+        .menu-bar ul li:hover .user ul {
+            display: block;
+        }
+
+        .menu-bar ul li:hover .menu ul li,
+        .menu-bar ul li:hover .user ul li {
+            width: 150px;
+            padding: 10px;
+            border-bottom: 1px dotted #fff;
+            background: transparent;
+            border-radius: 0;
+            text-align: center;
+        }
+
+        .menu-bar ul li:hover .menu ul li:last-child,
+        .menu-bar ul li:hover .user ul li:last-child {
+            border-bottom: none;
+        }
+
+        .menu-bar ul li:hover .menu ul li a:hover,
+        .menu-bar ul li:hover .user ul li a:hover {
+            color: rgb(124, 17, 17);
+        }
+
+        .logo {
+            width: 10%;
+            height: 40px;
+            right: 0;
+            position: absolute;
+        }
+
+
+        .product {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 20%;
+            height: 30%;
+            border-radius: 15px;
+            background-color: rgb(241, 238, 238);
+            margin: 70px 50px 0 0;
+            font-size: 20px;
+        }
+
+        .food {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 15px;
+            margin: 20px 20px 5px 20px;
+        }
+
+        .section {
+            margin-bottom: 50px;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            margin-left: 12%;
+        }
+
+        h1,
+        h2,
+        h3 {
+            text-align: center;
+        }
+
+
+        .cart-icon {
+            display: inline-block;
+            width: 18px;
+            height: 18px;
+            background-image: url("cart-icon.png");
+            background-size: contain;
+        }
+
+        footer {
+            background: #f18b05;
+            color: white;
+            text-align: center;
+            padding: 10px;
+        }
+
+        .intro {
+            background-color: #fda025;
+            width: 100%;
+            color: white;
+            padding: 2% 0;
+            font-size: xx-large;
+        }
+
+        h2 {
+            padding: 30px;
+        }
+
+        .submit {
+            margin-left: 47%;
+            margin-bottom: 3%;
+            background-color: #f18b05;
+            width: 70px;
+            border: 0;
+            padding: 10px;
+            color: white;
+            border-radius: 10px;
+        }
+
+        .user-icon {
+            display: inline-block;
+            width: 18px;
+            height: 18px;
+            background-image: url("user.png");
+            background-size: contain;
+        }
+
+        .product .button {
+            display: block;
+            width: 130px;
+            padding: 12px;
+            background: #f18b05;
+            color: #fff;
+            border: 0;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .product .button:hover {
+            background: #fda735;
+        }
+
+        .rate_sec {
+            display: flex;
+            flex-direction: row;
+            margin-left: 20%;
+            margin-bottom: 50px;
+        }
+
+        .show_rate {
+            display: flex;
+            flex-direction: column;
+            width: 40%;
+            text-align: center;
+        }
+
+        .total-rate {
+            width: 75%;
+            border-radius: 15px;
+            background-color: rgb(241, 238, 238);
+            font-size: 20px;
+            padding: 20px;
+            padding-left: 50px;
+        }
+
+        .rating3 {
+            position: absolute;
+            width: 35px;
+            height: 50px;
+            cursor: pointer;
+            transform: translateX(52px);
+            opacity: 0;
+            z-index: 5;
+        }
+
+        .star3 {
+            font-family: FontAwesome;
+            font-size: 30px;
+            color: #FBB202;
+            cursor: pointer;
+            margin: 0;
+            padding: 5px;
+        }
+
+        .star3::after {
+            content: '\f005';
+        }
+
+        .rate {
+            width: 75%;
+            height: auto;
+            border-radius: 15px;
+            background-color: rgb(241, 238, 238);
+            font-size: 18px;
+            padding: 10px;
+            margin: 30px 10px 10px 0;
+        }
+
+        .show {
+            position: absolute;
+            width: 35px;
+            height: 50px;
+            cursor: pointer;
+            transform: translateX(52px);
+            opacity: 0;
+            z-index: 5;
+        }
+
+        .star2 {
+            display: inline-block;
+            font-family: FontAwesome;
+            font-size: 30px;
+            color: #FBB202;
+            cursor: pointer;
+            margin: 0;
+            padding: 0;
+        }
+
+        .star2::after {
+            content: '\f005';
+        }
+
+        .rating {
+            width: 35%;
+            padding: 20px;
+            border-radius: 15px;
+            background-color: rgb(241, 238, 238);
+            text-align: center;
+        }
+
+        .rating .emo {
+            position: absolute;
+            font-family: FontAwesome;
+            color: #f18b05;
+            font-size: 90px;
+            transform: translateX(-50%);
+            margin-bottom: 20px;
+        }
+
+        .rating .emo:after {
+            content: '\f118';
+        }
+
+        .rating div input {
+            position: absolute;
+            width: 25px;
+            height: 50px;
+            cursor: pointer;
+            transform: translateX(52px);
+            opacity: 0;
+            z-index: 5;
+        }
+
+        .rating input:nth-of-type(4):checked~.emo:after,
+        .rating input:nth-of-type(4):hover~.emo:after,
+        .rating input:nth-of-type(3):checked~.emo:after,
+        .rating input:nth-of-type(3):hover~.emo:after {
+            content: '\f11a';
+        }
+
+        .rating input:nth-of-type(1):checked~.emo:after,
+        .rating input:nth-of-type(1):hover~.emo:after,
+        .rating input:nth-of-type(2):checked~.emo:after,
+        .rating input:nth-of-type(2):hover~.emo:after {
+            content: '\f118';
+        }
+
+
+
+        .rating input:checked~.star:after,
+        .rating input:hover~.star:after {
+            content: '\f005';
+        }
+
+        .rating .star {
+            display: inline-block;
+            font-family: FontAwesome;
+            font-size: 42px;
+            color: #FBB202;
+            cursor: pointer;
+            margin: 3px;
+        }
+
+        .rating .star:after {
+            content: '\f006';
+        }
+
+        .rating .star:hover~.star:after,
+        .rating .star:hover:after {
+            content: '\f005';
+        }
+
+        .box {
+            overflow: auto;
+            text-align: right;
+            font-size: 12px;
+            margin-top: 5px;
+            margin-right: 10px;
+        }
+
+        .sub {
+            background-color: #f18b05;
+            width: 70px;
+            border: 0;
+            color: white;
+            border-radius: 10px;
+            padding: 10px;
+            margin-top: 2px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="menu-bar">
+        <img src="logo.jpg" class="logo">
+        <ul>
+            <li><a href="cart.php"><span class="cart-icon"></span></a></li>
+            <li><a href="#"><span class="user-icon"></span></a>
+                <div class="user">
+                    <ul>
+                        <li><a href="Logout.php">تسجيل الخروج</a></li>
+                        <li><a href="ChangePassword.php">تغيير كلمة السر</a></li>
+                        <li><a href="DeleteAccount.php">حذف الحساب</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="Home.php#cont">من نحن</a></li>
+            <li class="rest"><a href=" #">المطاعم</a>
+                <div class="menu">
+                    <ul>
+                        <li><a href="Zaza.php">مطعم ظاظا</a></li>
+                        <li><a href="Village.php">مطعم القرية الطلابية</a></li>
+                        <li><a href="Espresso.php">مطعم اسبريسو</a></li>
+                        <li><a href="Eastern.php">مطعم الشرقي</a></li>
+                        <li><a href="Western.php">مطعم الغربي</a></li>
+                        <li><a href="Medicine.php">مطعم الطب</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="Home.php">الصفحة الرئيسية</a></li>
+        </ul>
+    </nav>
+    <div class="intro">
+        <h1>مطاعم ظاظا</h1>
+        <h3>الموقع : بجانب مبنى مطاعم الجامعة</h3>
+    </div>
+    <div class="section">
+        <?php
+        if (!$database = mysqli_connect("localhost", "root", "12345678", "hubreak2_db"))
+            die("Sorry, could not connect to the server.");
+        extract($_POST);
+        $query = "select products.*,resturantproducts.resturantId from products join resturantproducts on products.ID = productId WHERE resturantproducts.resturantId = 5 ";
+        $result = mysqli_query($database, $query);
+        while ($row = mysqli_fetch_row($result)) {
+            print("<form class='product' method='post' action='update_zaza.php'>");// onclick='myFunction()
+            $x = 0;
+            foreach ($row as $value) {
+                if ($x == 0)
+                    print("<img src='$value' class='food'></img>");
+                elseif ($x == 1)
+                    print("<input type='hidden' name='ID' value='$value'>");
+                elseif ($x == 4)
+                    print("<input type='hidden' name='restID' value='$value'>");
+                else {
+                    print("<span >$value</span>");
+                }
+                $x++;
+            }
+            print("<span class='count'><input type='text' style='margin-right: 1px;' 
+            name='new_price' size='5' autocomplete='off'> : السعر الجديد</span><br>
+                        <input type='submit' class='button' value='تعديل السعر'></input><br>");
+            print("</form>");
+        }
+        mysqli_close($database);
+        ?>
+    </div>
+    <footer>
+        <p>Developed By</p>
+        <p>HU Break Team &copy;</p>
+    </footer>
+
+    <script>
+        function myFunction() {
+            alert("تم التعديل بنجاح!");
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ admin/update_zaza.php
@@ -0,0 +1,9 @@
+<?php
+    if (!$database = mysqli_connect("localhost", "root", "12345678", "hubreak2_db"))
+        die("Sorry, could not connect to the server.");
+    extract($_POST);
+    $query2 = "update products set Price = $new_price where products.ID = $ID";
+    mysqli_query($database, $query2);
+    mysqli_close($database);
+    header("Location: change_zaza.php");
+    ?>
