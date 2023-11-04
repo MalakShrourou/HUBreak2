fe64cb179792ea942088167bc22c436782476529
MalakShrourou <malakfikri36@gmail.com>
2023-11-04 22:53:38 +0300

    deleted:    admin/AddItems.html 	deleted:    admin/AddItems.jpg 	deleted:    admin/Admin.html 	deleted:    admin/BrowseReviews.html 	deleted:    admin/BrowseReviews.jpg 	deleted:    admin/ChangePrices.html 	deleted:    admin/ChangePrices.jpg 	deleted:    admin/DeleteItems.html 	deleted:    admin/DeleteItems.jpg 	deleted:    admin/EasternHurricane.jpg 	deleted:    admin/Espresso.jpg 	deleted:    admin/Medicine.jpg 	deleted:    admin/SubmitPosters.jpg 	deleted:    admin/Village.jpg 	deleted:    admin/WesternHurricane.jpg 	deleted:    admin/Zaza.jpg deleted:    admin/add_zaza.php 	deleted:    admin/cart-icon.png 	deleted:    admin/change_zaza.php 	deleted:    admin/cover.jpg 	deleted:    admin/delete_zaza.php 	deleted:    admin/insert_zaza.php 	deleted:    admin/login.php 	deleted:    admin/logo.jpg 	deleted:    admin/remove_zaza.php 	deleted:    admin/update_zaza.php 	deleted:    admin/user.png 	deleted:    admin/welcome3.png

--- admin/add_zaza.php
+++ admin/add_zaza.php
@@ -9,252 +9,247 @@ session_start();
     <meta charset=utf8>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <style>
-    * {
-        margin: 0;
-        padding: 0;
-        font-family: 'Times New Roman', Times, serif;
-        box-sizing: border-box;
-    }
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', Times, serif;
+            box-sizing: border-box;
+        }
 
-    body {
-        color: rgb(124, 17, 17);
-        background-attachment: fixed;
-    }
+        body {
+            color: rgb(124, 17, 17);
+            background-attachment: fixed;
+        }
 
-    .menu-bar {
-        background: #f18b05;
-        text-align: right;
-        height: 40px;
-        margin-right: 10%;
-        padding-right: 5px;
-        font-size: large;
-    }
+        .menu-bar {
+            background: #f18b05;
+            text-align: right;
+            height: 40px;
+            margin-right: 10%;
+            padding-right: 5px;
+            font-size: large;
+        }
 
-    .menu-bar ul {
-        display: inline-flex;
-        list-style: none;
-        color: #fff;
-    }
+        .menu-bar ul {
+            display: inline-flex;
+            list-style: none;
+            color: #fff;
+        }
 
-    .menu-bar ul li {
-        width: 120px;
-        margin: 0px;
-        padding: 10px 0px;
-        text-align: center;
-    }
+        .menu-bar ul li {
+            width: 120px;
+            margin: 0px;
+            padding: 10px 0px;
+            text-align: center;
+        }
 
-    .menu-bar ul li a {
-        text-decoration: none;
-        color: #fff;
-        padding-right: 10px;
-    }
+        .menu-bar ul li a {
+            text-decoration: none;
+            color: #fff;
+            padding-right: 10px;
+        }
 
-    .menu,
-    .user {
-        display: none;
-    }
+        .menu,
+        .user {
+            display: none;
+        }
 
-    .menu-bar ul li:hover {
-        background-color: #fda025;
-    }
+        .menu-bar ul li:hover {
+            background-color: #fda025;
+        }
 
-    .menu-bar ul li:hover .menu,
-    .menu-bar ul li:hover .user {
-        display: block;
-        position: absolute;
-        background-color: #FBB202;
-        margin-top: 10px;
-        margin-left: -20px;
-    }
+        .menu-bar ul li:hover .menu,
+        .menu-bar ul li:hover .user {
+            display: block;
+            position: absolute;
+            background-color: #FBB202;
+            margin-top: 10px;
+            margin-left: -20px;
+        }
 
-    .menu-bar ul li:hover .menu ul,
-    .menu-bar ul li:hover .user ul {
-        display: block;
-    }
+        .menu-bar ul li:hover .menu ul,
+        .menu-bar ul li:hover .user ul {
+            display: block;
+        }
 
-    .menu-bar ul li:hover .menu ul li,
-    .menu-bar ul li:hover .user ul li {
-        width: 150px;
-        padding: 10px;
-        border-bottom: 1px dotted #fff;
-        background: transparent;
-        border-radius: 0;
-        text-align: center;
-    }
+        .menu-bar ul li:hover .menu ul li,
+        .menu-bar ul li:hover .user ul li {
+            width: 150px;
+            padding: 10px;
+            border-bottom: 1px dotted #fff;
+            background: transparent;
+            border-radius: 0;
+            text-align: center;
+        }
 
-    .menu-bar ul li:hover .menu ul li:last-child,
-    .menu-bar ul li:hover .user ul li:last-child {
-        border-bottom: none;
-    }
+        .menu-bar ul li:hover .menu ul li:last-child,
+        .menu-bar ul li:hover .user ul li:last-child {
+            border-bottom: none;
+        }
 
-    .menu-bar ul li:hover .menu ul li a:hover,
-    .menu-bar ul li:hover .user ul li a:hover {
-        color: rgb(124, 17, 17);
-    }
+        .menu-bar ul li:hover .menu ul li a:hover,
+        .menu-bar ul li:hover .user ul li a:hover {
+            color: rgb(124, 17, 17);
+        }
 
-    .logo {
-        width: 10%;
-        height: 40px;
-        right: 0;
-        position: absolute;
-    }
-
-    .product {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 20%;
-        height: 30%;
-        border-radius: 15px;
-        background-color: rgb(241, 238, 238);
-        margin: 70px 50px 0 0;
-        font-size: 20px;
-    }
-
-    .food {
-        width: 200px;
-        height: 200px;
-        object-fit: cover;
-        border-radius: 15px;
-        margin: 20px 20px 5px 20px;
-    }
-
-    .section {
-        margin-bottom: 50px;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        margin-left: 12%;
-    }
-
-    h1,
-    h2,
-    h3 {
-        text-align: center;
-    }
+        .logo {
+            width: 10%;
+            height: 40px;
+            right: 0;
+            position: absolute;
+        }
 
 
-    .cart-icon {
-        display: inline-block;
-        width: 18px;
-        height: 18px;
-        background-image: url("cart-icon.png");
-        background-size: contain;
-    }
+        .product {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 20%;
+            height: 30%;
+            border-radius: 15px;
+            background-color: rgb(241, 238, 238);
+            margin: 70px 50px 0 0;
+            font-size: 20px;
+        }
 
-    footer {
-        background: #f18b05;
-        color: white;
-        text-align: center;
-        padding: 10px;
-    }
+        .food {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 15px;
+            margin: 20px 20px 5px 20px;
+        }
 
-    .intro {
-        background-color: #fda025;
-        width: 100%;
-        color: white;
-        padding: 2% 0;
-        font-size: xx-large;
-    }
+        .section {
+            margin-bottom: 50px;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            margin-left: 12%;
+        }
 
-    h2 {
-        padding: 30px;
-    }
+        h1,
+        h2,
+        h3 {
+            text-align: center;
+        }
 
-    .submit {
-        margin-left: 47%;
-        margin-bottom: 3%;
-        background-color: #f18b05;
-        width: 70px;
-        border: 0;
-        padding: 10px;
-        color: white;
-        border-radius: 10px;
-    }
 
-    .user-icon {
-        display: inline-block;
-        width: 18px;
-        height: 18px;
-        background-image: url("user.png");
-        background-size: contain;
-    }
+        .cart-icon {
+            display: inline-block;
+            width: 18px;
+            height: 18px;
+            background-image: url("cart-icon.png");
+            background-size: contain;
+        }
 
-    .product .button {
-        display: block;
-        width: 130px;
-        padding: 12px;
-        background: #f18b05;
-        color: #fff;
-        border: 0;
-        border-radius: 5px;
-        cursor: pointer;
-    }
+        footer {
+            background: #f18b05;
+            color: white;
+            text-align: center;
+            padding: 10px;
+        }
 
-    .product .button:hover {
-        background: #fda735;
-    }
+        .intro {
+            background-color: #fda025;
+            width: 100%;
+            color: white;
+            padding: 2% 0;
+            font-size: xx-large;
+        }
 
-    .box {
-        overflow: auto;
-        text-align: right;
-        font-size: 12px;
-        margin-top: 5px;
-        margin-right: 10px;
-    }
+        h2 {
+            padding: 30px;
+        }
 
-    .sub {
-        background-color: #f18b05;
-        width: 70px;
-        border: 0;
-        color: white;
-        border-radius: 10px;
-        padding: 10px;
-        margin-top: 2px;
-    }
+        .submit {
+            margin-left: 47%;
+            margin-bottom: 3%;
+            background-color: #f18b05;
+            width: 70px;
+            border: 0;
+            padding: 10px;
+            color: white;
+            border-radius: 10px;
+        }
 
-    .container {
-        max-width: 400px;
-        margin: 20px auto;
-        padding: 20px;
-        border: 1px solid #ccc;
-        background-color: #f9f9f9;
-        text-align: right;
-    }
+        .user-icon {
+            display: inline-block;
+            width: 18px;
+            height: 18px;
+            background-image: url("user.png");
+            background-size: contain;
+        }
 
-    .form-group {
-        margin-bottom: 15px;
-        text-align: center;
-    }
+        .product .button {
+            display: block;
+            width: 130px;
+            padding: 12px;
+            background: #f18b05;
+            color: #fff;
+            border: 0;
+            border-radius: 5px;
+            cursor: pointer;
+        }
 
-    .form-group label {
-        display: block;
-        margin-bottom: 5px;
-        text-align: right;
-    }
+        .product .button:hover {
+            background: #fda735;
+        }
 
-    .form-group input[type="file"],
-    .form-group textarea {
-        width: 100%;
-        padding: 10px;
-        margin-bottom: 10px;
-        text-align: right;
-    }
+        .box {
+            overflow: auto;
+            text-align: right;
+            font-size: 12px;
+            margin-top: 5px;
+            margin-right: 10px;
+        }
 
-    .form-group input[type="submit"] {
-        background-color: #f18b05;
-        color: white;
-        font-weight: bold;
-        border: none;
-        padding: 10px 20px;
-        cursor: pointer;
-    }
+        .sub {
+            background-color: #f18b05;
+            width: 70px;
+            border: 0;
+            color: white;
+            border-radius: 10px;
+            padding: 10px;
+            margin-top: 2px;
+        }
 
-    .button {
-        font-size: large;
-        font-weight: bold;
-        border-radius: 5px;
-    }
+        .container {
+            max-width: 400px;
+            margin: 20px auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            background-color: #f9f9f9;
+            text-align: right;
+        }
+        .form-group {
+            margin-bottom: 15px;
+            text-align: center;
+        }
+        .form-group label {
+            display: block;
+            margin-bottom: 5px;
+            text-align: right;
+        }
+        .form-group input[type="file"], .form-group textarea {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            text-align: right;
+        }
+        .form-group input[type="submit"] {
+            background-color: #f18b05;
+            color: white;
+            font-weight: bold;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+        }
+        .button {
+            font-size: large;
+            font-weight: bold;
+            border-radius: 5px;
+        }
     </style>
 </head>
 
@@ -299,36 +294,23 @@ session_start();
         <form action="insert_zaza.php" method="post">
             <div class="form-group">
                 <label for="image">أرفق صورة الوجبة</label>
-                <input type="file" name="meal_image" id="image" accept="image/*">
+                <input type="file" name="meal_image" id="image" accept="image/*" >
             </div>
             <p>
-                <label>
-                    <input type="text" placeholder="name" name="meal_name" required>
-                </label>اسم الوجبة
-            </p>
+				<label>
+					<input type="text" placeholder="name" name="meal_name" autocomplete="off" required>
+				</label>اسم الوجبة
+			</p>
             <br>
             <p>
-                <label>
-                    <input type="text" placeholder="price" name="meal_price" required>
-                </label>سعر الوجبة
-            </p>
+				<label>
+					<input type="text" placeholder="price" name="meal_price" autocomplete="off" required>
+				</label>سعر الوجبة
+			</p>
             <br>
             <div class="form-group">
-                <input type="submit" value="إضافة" onclick="myFunction()">
-                <label>
-                    <input type="text" placeholder="name" name="meal_name" autocomplete="off" required>
-                </label>اسم الوجبة
-                </p>
-                <br>
-                <p>
-                    <label>
-                        <input type="text" placeholder="price" name="meal_price" autocomplete="off" required>
-                    </label>سعر الوجبة
-                </p>
-                <br>
-                <div class="form-group">
-                    <input type="submit" class="button" value="إضافة">
-                </div>
+                <input type="submit" class="button" value="إضافة">
+            </div>
         </form>
     </div>
     <footer>
@@ -337,9 +319,9 @@ session_start();
     </footer>
 
     <script>
-    function myFunction() {
-        alert("تمت الإضافة بنجاح!");
-    }
+        function myFunction() {
+            alert("تمت الإضافة بنجاح!");
+        }
     </script>
 
 </body>
---
+++ admin/login.php
@@ -0,0 +1,189 @@
+<?php
+session_start();
+
+if (!$database = mysqli_connect("localhost", "root", "12345678"))
+    die("Sorry, could not connect to the server.");
+if (!mysqli_select_db($database, "hubreak2_db"))
+    die("Sorry, could not find database.");
+extract($_POST);
+$error = "";
+if (isset($submit)) {
+    $query = "SELECT * FROM resturants WHERE username = '$username'";
+    if (!$result = mysqli_query($database, $query))
+        die("wrong query");
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        if ($password == $row['password']) {
+            $_SESSION['username'] = $username;
+            $error = "";
+            header("Location:Home.php");
+        } else {
+            $error = "Wrong Password";
+        }
+    } else {
+        $error = "Invalid username";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HU Break</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <style>
+        @import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+        * {
+            font-family: 'Times New Roman', Times, serif;
+        }
+
+        body {
+            background-image: url("cover.jpg");
+        }
+
+        img {
+            margin: 1% auto 0 30%;
+        }
+
+        .login-page {
+            width: 460px;
+            padding: 10px;
+            margin: 0 auto 0 30%;
+        }
+
+        .form {
+            position: relative;
+            background: #FFFFFF;
+            max-width: 360px;
+            padding: 45px;
+            text-align: center;
+            box-shadow: -30px 30px 20px rgba(0, 0, 0, 0.3);
+            margin-top: -10px;
+            border-radius: 30px;
+        }
+
+        .form input {
+            outline: 0;
+            background: #f2f2f2;
+            width: 90%;
+            margin: 0 0 15px;
+            padding: 15px;
+            box-sizing: border-box;
+            font-size: 14px;
+            border-radius: 30px;
+            border: 0;
+        }
+
+        .form button {
+            background: orange;
+            border: 0;
+            width: 40%;
+            padding: 15px;
+            color: #FFFFFF;
+            font-size: 14px;
+            -webkit-transition: all 0.3 ease;
+            transition: all 0.3 ease;
+            cursor: pointer;
+            border-radius: 30px;
+        }
+
+        .form button:hover,
+        .form button:active,
+        .form button:focus {
+            background: #b88715;
+        }
+
+        .form .message {
+            margin: 15px 0 0;
+            color: #b3b3b3;
+            font-size: 12px;
+        }
+
+        .form .message a {
+            color: orange;
+            text-decoration: none;
+        }
+
+        .form .register-form {
+            display: none;
+        }
+
+        .container {
+            position: relative;
+            z-index: 1;
+            max-width: 300px;
+            margin: 0 auto;
+        }
+
+        .container:before,
+        .container:after {
+            content: "";
+            display: block;
+            clear: both;
+        }
+
+        .container .info {
+            margin: 50px auto;
+            text-align: center;
+        }
+
+        .container .info h1 {
+            margin: 0 0 15px;
+            padding: 0;
+            font-size: 36px;
+            font-weight: 300;
+            color: #1a1a1a;
+        }
+
+        .container .info span {
+            color: #4d4d4d;
+            font-size: 12px;
+        }
+
+        .container .info span a {
+            color: #000000;
+            text-decoration: none;
+        }
+
+        .container .info span .fa {
+            color: #EF3B3A;
+        }
+
+        .error {
+            color: red;
+            font-weight: bold;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <img src="welcome3.png" height="150" width="500">
+    <div class="login-page">
+        <div class="form">
+            <form class="login-form" method="POST" autocomplete="off">
+                <input type="test" name="username" placeholder="Username" required />
+                <input type="password" name="password" placeholder="Password (must containe 8 digits)" required
+                    spellcheck="false" style="margin-left: -10px; " required id="id_password" />
+                <i class="far fa-eye" id="togglePassword" style="margin-left: -35px; cursor: pointer;"></i>
+                <script>
+                    const togglePassword = document.querySelector('#togglePassword');
+                    const password = document.querySelector('#id_password');
+                    togglePassword.addEventListener('click', function (e) {
+                        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+                        password.setAttribute('type', type);
+                        this.classList.toggle('fa-eye-slash');
+                    });
+                </script>
+                <p class="error">
+                    <?php echo $error; ?>
+                </p>
+                <button name="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
