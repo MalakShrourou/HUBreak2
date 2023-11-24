0c8c2861a74884b7939263532ce9b0cab80fd9eb
MalakShrourou <malakfikri36@gmail.com>
2023-11-25 00:56:27 +0300

    new file:   Customer/Time.php 	modified:   Customer/placeorder.php 	modified:   hubreak2_db.sql

---
+++ Customer/Time.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cart</title>
+    <meta charset="UTF-8">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', Times, serif;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-attachment: fixed;
+            background-image: url("cover.jpg");
+        }
+
+        .section {
+            width: 30%;
+            font-size: 28px;
+            background-color: rgba(255, 255, 255, .7);
+            height: auto;
+            margin: 50px 150px 30px 20px;
+            border-radius: 15px;
+            padding-bottom: 20px;
+        }
+
+        h4 {
+            background-color: #f18b05;
+            text-align: center;
+            padding: 5px;
+            width: 50%;
+            margin-left: 100px;
+            margin-top: 0;
+            color: white;
+        }
+
+        .table {
+            font-size: 22px;
+            width: 100%;
+            color: rgb(124, 17, 17);
+            height: auto;
+        }
+
+        .table td {
+            text-align: center;
+        }
+
+        tr {
+            height: 50px;
+        }
+
+        .center {
+            display: flex;
+            flex-direction: row-reverse;
+            flex-wrap: wrap;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="center">
+        <?php
+        if (!$database = mysqli_connect("localhost", "root", "12345678", "hubreak2_db"))
+            die("Sorry, could not connect to the server.");
+        extract($_POST);
+        $query = "SELECT DISTINCT ResturantID FROM carts";
+        $result = mysqli_query($database, $query);
+        while ($row = mysqli_fetch_row($result)) {
+            foreach ($row as $value) {
+                if ($value == 1) {
+                    print("<div class='section'><h4>مطاعم الشرقي</h4>");
+                    $Q1 = "SELECT TotalPrice,Time FROM carts WHERE ResturantID=1";
+                    $result1 = mysqli_query($database, $Q1);
+                    print("<table class='table'>");
+                    while ($row = mysqli_fetch_row($result1)) {
+                        $x = 0;
+                        foreach ($row as $value) {
+                            if ($x == 0) {
+                                print("<tr><td>$value JD</td>");
+                                print("<td>المبلغ الإجمالي</td></tr>");
+                            } else {
+                                $dbSessionDurationTime = $value * 60 * 1000;
+                                print("<tr><td id='demo'></td>");
+                                print("<td>الوقت المتبقي</td></tr></table></div>");
+                            }
+                            $x++;
+                        }
+                    }
+                } elseif ($value == 2) {
+                    print("<div class='section'><h4>مطاعم اسبريسو</h4>");
+                    $Q2 = "SELECT TotalPrice,Time FROM carts WHERE ResturantID=2";
+                    $result2 = mysqli_query($database, $Q2);
+                    print("<table class='table'>");
+                    while ($row2 = mysqli_fetch_row($result2)) {
+                        $x = 0;
+                        foreach ($row2 as $value) {
+                            if ($x == 0) {
+                                print("<tr><td>$value JD</td>");
+                                print("<td>المبلغ الإجمالي</td></tr>");
+                            } else {
+                                $dbSessionDurationTime2 = $value * 60 * 1000;
+                                print("<tr><td id='demo'></td>");
+                                print("<td>الوقت المتبقي</td></tr></table></div>");
+                            }
+                            $x++;
+                        }
+                    }
+                } elseif ($value == 3) {
+                    print("<div class='section'><h4>مطاعم الطب</h4>");
+                    $Q3 = "SELECT TotalPrice,Time FROM carts WHERE ResturantID=3";
+                    $result3 = mysqli_query($database, $Q3);
+                    print("<table class='table'>");
+                    while ($row3 = mysqli_fetch_row($result3)) {
+                        $x = 0;
+                        foreach ($row3 as $value) {
+                            if ($x == 0) {
+                                print("<tr><td>$value JD</td>");
+                                print("<td>المبلغ الإجمالي</td></tr>");
+                            } else {
+                                $dbSessionDurationTime = $value * 60 * 1000;
+                                print("<tr><td id='demo'></td>");
+                                print("<td>الوقت المتبقي</td></tr></table></div>");
+                            }
+                            $x++;
+                        }
+                    }
+                } elseif ($value == 4) {
+                    print("<div class='section'><h4>مطاعم القرية الطلابية</h4>");
+                    $Q4 = "SELECT TotalPrice,Time FROM carts WHERE ResturantID=4";
+                    $result4 = mysqli_query($database, $Q4);
+                    print("<table class='table'>");
+                    while ($row4 = mysqli_fetch_row($result4)) {
+                        $x = 0;
+                        foreach ($row4 as $value) {
+                            if ($x == 0) {
+                                print("<tr><td>$value JD</td>");
+                                print("<td>المبلغ الإجمالي</td></tr>");
+                            } else {
+                                $dbSessionDurationTime = $value * 60 * 1000;
+                                print("<tr><td id='demo'></td>");
+                                print("<td>الوقت المتبقي</td></tr></table></div>");
+                            }
+                            $x++;
+                        }
+                    }
+                } elseif ($value == 5) {
+                    print("<div class='section'><h4>مطاعم ظاظا</h4>");
+                    $Q5 = "SELECT TotalPrice,Time FROM carts WHERE ResturantID=5";
+                    $result5 = mysqli_query($database, $Q5);
+                    print("<table class='table'>");
+                    while ($row5 = mysqli_fetch_row($result5)) {
+                        $x = 0;
+                        foreach ($row5 as $value) {
+                            if ($x == 0) {
+                                print("<tr><td>$value JD</td>");
+                                print("<td>المبلغ الإجمالي</td></tr>");
+                            } else {
+                                $dbSessionDurationTime = $value * 60 * 1000;
+                                print("<tr><td id='demo5'></td>");
+                                print("<td>الوقت المتبقي</td></tr></table></div>");
+                            }
+                            $x++;
+                        }
+                    }
+                } elseif ($value == 6) {
+                    print("<div class='section'><h4>مطاعم الغربي</h4>");
+                    $Q6 = "SELECT TotalPrice,Time FROM carts WHERE ResturantID=6";
+                    $result6 = mysqli_query($database, $Q6);
+                    print("<table class='table'>");
+                    while ($row6 = mysqli_fetch_row($result6)) {
+                        $x = 0;
+                        foreach ($row6 as $value) {
+                            if ($x == 0) {
+                                print("<tr><td>$value JD</td>");
+                                print("<td>المبلغ الإجمالي</td></tr>");
+                            } else {
+                                $dbSessionDurationTime = $value * 60 * 1000;
+                                print("<tr><td id='demo'></td>");
+                                print("<td>الوقت المتبقي</td></tr></table></div>");
+                            }
+                            $x++;
+                        }
+                    }
+                }
+            }
+        }
+        ?>
+    </div>
+    <script type="text/javascript">
+        var millis = <?php echo $dbSessionDurationTime; ?>
+
+        function displaytimer() {
+            var hours = Math.floor(millis / 36e5),
+                mins = Math.floor((millis % 36e5) / 6e4),
+                secs = Math.floor((millis % 6e4) / 1000);
+            var remainingTime = mins + ':' + secs + ' minutes';
+            document.getElementById("demo").innerHTML = mins + " m " + secs + " s ";
+        }
+        setInterval(function () {
+            millis -= 1000;
+            displaytimer();
+        }, 1000);
+    </script>
+</body>
+
+</html>
--- Customer/placeorder.php
+++ Customer/placeorder.php
@@ -15,25 +15,81 @@ $result = mysqli_query($database, $query);
 while ($row = mysqli_fetch_row($result)) {
     foreach ($row as $value) {
         if ($value == 1) {
-            $Q1 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description) VALUES ('$ID',1,'$total1','$desc1')";
+            $qu1 = "SELECT Time from Orders join products where Orders.ProductID= Products.ID AND resturantID=1";
+            $max = 0;
+            $result1 = mysqli_query($database, $qu1);
+            while ($row = mysqli_fetch_row($result1)) {
+                foreach ($row as $value) {
+                    if ($value > $max)
+                        $max = $value;
+                }
+            }
+            $Q1 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description,Time) VALUES ('$ID',1,'$total1','$desc1','$max')";
             $result1 = mysqli_query($database, $Q1);
         } elseif ($value == 2) {
-            $Q2 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description) VALUES ('$ID',2,'$total2','$desc2')";
+            $qu2 = "SELECT Time from Orders join products where Orders.ProductID= Products.ID AND resturantID=2";
+            $max = 0;
+            $result2 = mysqli_query($database, $qu2);
+            while ($row = mysqli_fetch_row($result2)) {
+                foreach ($row as $value) {
+                    if ($value > $max)
+                        $max = $value;
+                }
+            }
+            $Q2 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description,Time) VALUES ('$ID',2,'$total2','$desc2','$max')";
             $result2 = mysqli_query($database, $Q2);
         } elseif ($value == 3) {
-            $Q3 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description) VALUES ('$ID',3,'$total3','$desc3')";
+            $qu3 = "SELECT Time from Orders join products where Orders.ProductID= Products.ID AND resturantID=3";
+            $max = 0;
+            $result3 = mysqli_query($database, $qu3);
+            while ($row = mysqli_fetch_row($result3)) {
+                foreach ($row as $value) {
+                    if ($value > $max)
+                        $max = $value;
+                }
+            }
+            $Q3 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description,Time) VALUES ('$ID',3,'$total3','$desc3','$max')";
             $result3 = mysqli_query($database, $Q3);
         } elseif ($value == 4) {
-            $Q4 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description) VALUES ('$ID',4,'$total4','$desc4')";
+            $qu4 = "SELECT Time from Orders join products where Orders.ProductID= Products.ID AND resturantID=4";
+            $max = 0;
+            $result4 = mysqli_query($database, $qu4);
+            while ($row = mysqli_fetch_row($result4)) {
+                foreach ($row as $value) {
+                    if ($value > $max)
+                        $max = $value;
+                }
+            }
+            $Q4 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description,Time) VALUES ('$ID',4,'$total4','$desc4','$max')";
             $result4 = mysqli_query($database, $Q4);
         } elseif ($value == 5) {
-            $Q5 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description) VALUES ('$ID',5,'$total5','$desc5')";
+            $qu5 = "SELECT Time from Orders join products where Orders.ProductID= Products.ID AND resturantID=5";
+            $max = 0;
+            $result5 = mysqli_query($database, $qu5);
+            while ($row = mysqli_fetch_row($result5)) {
+                foreach ($row as $value) {
+                    if ($value > $max)
+                        $max = $value;
+                }
+            }
+            $Q5 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description,Time) VALUES ('$ID',5,'$total5','$desc5','$max')";
             $result5 = mysqli_query($database, $Q5);
         } elseif ($value == 6) {
-            $Q6 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description) VALUES ('$ID',6,'$total6','$desc6')";
+            $qu6 = "SELECT Time from Orders join products where Orders.ProductID= Products.ID AND resturantID=6";
+            $max = 0;
+            $result6 = mysqli_query($database, $qu6);
+            while ($row = mysqli_fetch_row($result6)) {
+                foreach ($row as $value) {
+                    if ($value > $max)
+                        $max = $value;
+                }
+            }
+            $Q6 = "INSERT INTO carts(CustomerID,ResturantID,TotalPrice,Description,Time) VALUES ('$ID',6,'$total6','$desc6','$max')";
             $result6 = mysqli_query($database, $Q6);
         }
     }
 }
 mysqli_close($database);
+header("Location:Time.php");
+
 ?>
