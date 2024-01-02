e4c38d3eedabb8932a6d18cc8aa2366cda6b78ad
Baseljamal <70074852+Baseljamal@users.noreply.github.com>
2024-01-02 18:21:07 +0300

    Add files via upload

---
+++ Cashier/placeorder.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.php');
+    exit();
+}
+if (!$database = mysqli_connect("localhost", "root", "12345678", "hubreak2_db"))
+    die("Sorry, could not connect to the server.");
+extract($_POST);
+$ID = $_SESSION['ID'];
+$query = "SELECT DISTINCT resturantID FROM Orders";
+$result = mysqli_query($database, $query);
+while ($row = mysqli_fetch_row($result)) {
+    foreach ($row as $value) {
+        if ($value == 1) {
+            $Q1 = "INSERT INTO carts(customerID,ResturantID,TotalPrice,Description) VALUES (8,1,'$t','$desc')";
+            $result1 = mysqli_query($database, $Q1);
+        }
+        elseif ($value == 2) {
+            $Q1 = "INSERT INTO carts(customerID,ResturantID,TotalPrice,Description) VALUES (8,2,'$t','$desc')";
+            $result1 = mysqli_query($database, $Q1);
+        }
+        elseif ($value == 3) {
+            $Q1 = "INSERT INTO carts(customerID,ResturantID,TotalPrice,Description) VALUES (8,3,'$t','$desc')";
+            $result1 = mysqli_query($database, $Q1);
+        }
+        elseif ($value == 4) {
+            $Q1 = "INSERT INTO carts(customerID,ResturantID,TotalPrice,Description) VALUES (8,4,'$t','$desc')";
+            $result1 = mysqli_query($database, $Q1);
+        }
+        elseif ($value == 5) {
+            $Q1 = "INSERT INTO carts(customerID,ResturantID,TotalPrice,Description) VALUES (8,5,'$t','$desc')";
+            $result1 = mysqli_query($database, $Q1);
+        }
+        elseif ($value == 6) {
+            $Q1 = "INSERT INTO carts(customerID,ResturantID,TotalPrice,Description) VALUES (8,6,'$t','$desc')";
+            $result1 = mysqli_query($database, $Q1);
+        }
+    }
+}
+mysqli_close($database);
+header("Location:invoice.php");
+
+?>
