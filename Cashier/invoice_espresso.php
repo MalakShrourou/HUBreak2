9e009309cf9e72c58fdbdb8a1977bacf2f3a9e08
Baseljamal <70074852+Baseljamal@users.noreply.github.com>
2024-01-02 21:47:50 +0300

    Add files via upload

---
+++ Cashier/invoice_espresso.php
@@ -0,0 +1,213 @@
+<?php
+if (!$database = mysqli_connect("localhost", "root", "12345678"))
+    die("Sorry, could not connect to the server.");
+if (!mysqli_select_db($database, "hubreak2_db"))
+    die("Sorry, could not find database.");
+extract($_POST);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Invoice</title>
+    <style>
+        .result {
+            color: red;
+        }
+
+        .notes {
+            position: absolute;
+            right: 0px;
+        }
+
+        td {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="mt-3">
+        <div class="container-fluid">
+            <h4 class="text-center" style="color:#f18b05"> HUBreak </h4>
+            <h6 class="text-center">مطاعم اسبريسو</h6>
+            <div class="row" style="margin-right:370px;">
+                <div class="col-md-5  mt-4 ">
+                    <div role="alert" id="errorMsg" class="mt-5">
+                    </div>
+                </div>
+                <div class="col-md-7  mt-4" style="background-color:#f5f5f5;">
+                    <div class="p-4">
+                        <div class="text-center">
+                            <h4>فاتورة</h4>
+                        </div>
+                        <span class="mt-4">Time : </span><span class="mt-4" id="time"></span>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6 ">
+                                <span id="day"></span> : <span id="year"></span>
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6 text-right">
+                                <?php
+                                $q = "select ID from carts where ResturantID = 2 and frompos=1";
+                                $result = mysqli_query($database, $q);
+                                $row = mysqli_fetch_row($result);
+                                foreach ($row as $value)
+                                    print("<p>رقم الطلب : $value</p>");
+                                ?>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <table id="receipt_bill" class="table">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">الوجبة</th>
+                                        <th class="text-center">الكمية</th>
+                                        <th class="text-center">السعر</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="new">
+                                    <?php
+                                    $query = "select products.Name,Quantity,orders.price from products join orders on products.ID = orders.ProductID where ResturantID =2";
+                                    $result = mysqli_query($database, $query);
+                                    while ($row = mysqli_fetch_row($result)) {
+                                        print("<tr>");
+                                        $x = 0;
+                                        foreach ($row as $value) {
+                                            if ($x == 1) {
+                                                print("<td>$value</td>");
+                                                $quan = $value;
+                                            } else if ($x == 2) {
+                                                $p = $value * $quan;
+                                                print("<td>$p</td>");
+                                            } else
+                                                print("<td>$value</td>");
+                                            $x++;
+                                        }
+                                        print("</tr>");
+                                    }
+                                    ?>
+                                </tbody>
+                                <tfoot>
+                                    <div>
+                                        <?php
+                                        $qq = "select Description from carts where resturantId = 2";
+                                        $result = mysqli_query($database, $qq);
+                                        $row = mysqli_fetch_row($result);
+                                        if (mysqli_num_rows($result) > 0) {
+                                            print("<tr colspan=3><td class='notes'>الملاحظات : ");
+                                            foreach ($row as $value)
+                                                print($value);
+                                        }
+                                        print("</td></tr>");
+                                        ?>
+                                    </div>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+</body>
+
+</html>
+<script>
+    $(document).ready(function () {
+        $('#vegitable').change(function () {
+            var id = $(this).find(':selected')[0].id;
+            $.ajax({
+                method: 'POST',
+                url: 'fetch_product.php',
+                data: {
+                    id: id
+                },
+                dataType: 'json',
+                success: function (data) {
+                    $('#price').text(data.product_price);
+                }
+            });
+        });
+        var count = 1;
+        $('#add').on('click', function () {
+            var name = $('#vegitable').val();
+            var qty = $('#qty').val();
+            var price = $('#price').text();
+            if (qty == 0) {
+                var erroMsg =
+                    '<span class="alert alert-danger ml-5">Minimum Qty should be 1 or More than 1</span>';
+                $('#errorMsg').html(erroMsg).fadeOut(9000);
+            } else {
+                billFunction();
+            }
+
+            function billFunction() {
+                var total = 0;
+                $("#receipt_bill").each(function () {
+                    var total = price * qty;
+                    var subTotal = 0;
+                    subTotal += parseInt(total);
+                    var table = '<tr><td>' + count + '</td><td>' + name + '</td><td>' + qty +
+                        '</td><td>' + price +
+                        '</td><td><strong><input type="hidden" id="total" value="' + total + '">' +
+                        total + '</strong></td></tr>';
+                    $('#new').append(table)
+                    var total = 0;
+                    $('tbody tr td:last-child').each(function () {
+                        var value = parseInt($('#total', this).val());
+                        if (!isNaN(value)) {
+                            total += value;
+                        }
+                    });
+                    $('#subTotal').text(total);
+                    var Tax = (total * 5) / 100;
+                    $('#taxAmount').text(Tax.toFixed(2));
+                    var Subtotal = $('#subTotal').text();
+                    var taxAmount = $('#taxAmount').text();
+                    var totalPayment = parseFloat(Subtotal) + parseFloat(taxAmount);
+                    $('#totalPayment').text(totalPayment.toFixed(2));
+                });
+                count++;
+            }
+        });
+        var currentdate = new Date();
+        var datetime = currentdate.getDate() + "/" +
+            (currentdate.getMonth() + 1) + "/" +
+            currentdate.getFullYear();
+        $('#year').text(datetime);
+
+        // Code for extract Weekday     
+        function myFunction() {
+            var d = new Date();
+            var weekday = new Array(7);
+            weekday[0] = "Sunday";
+            weekday[1] = "Monday";
+            weekday[2] = "Tuesday";
+            weekday[3] = "Wednesday";
+            weekday[4] = "Thursday";
+            weekday[5] = "Friday";
+            weekday[6] = "Saturday";
+            var day = weekday[d.getDay()];
+            return day;
+        }
+        var day = myFunction();
+        $('#day').text(day);
+    });
+</script>
+
+<!-- // Code for TIME -->
+<script>
+    window.onload = displayClock();
+
+    function displayClock() {
+        var time = new Date().toLocaleTimeString();
+        document.getElementById("time").innerHTML = time;
+        setTimeout(displayClock, 1000);
+    }
+</script>
